16868699f3b5f89dd9fd38991f792073bc84ced7
Omar <omustardo@gmail.com>
2021-08-15 01:25:22 -0700

    Initial backend commit
    Routes are set up. File uploading is manually tested. Database skeleton is implemented, but it has no backing so there's no storage.

---
+++ backend/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/omustardo/memescene/backend/server"
+	"log"
+)
+
+var (
+	port = flag.Int("port", 13337, "port to serve from")
+
+	debug = flag.Bool("debug", false, "Whether to log extra for debugging")
+)
+
+func main() {
+	flag.Parse()
+
+	srv, err := server.New(*port, *debug)
+	if err != nil {
+		log.Fatalf("Failed to create server: %v", err)
+	}
+
+	fmt.Printf("Serving at http://localhost:%v\n", *port)
+	log.Fatal(srv.ListenAndServe())
+}
