2418cd85a02c47842b5890e0b6e6076a6254c37c
Charles Inwald <cinwald@gmail.com>
2021-08-16 19:32:05 -0400

    Fixed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
-
+import Header from "./common/Header"
+import MemeGenerator from "./MemeGenerator";
 const reactLogo = require("./../assets/img/react_logo.svg");
 // import "./../assets/scss/App.scss";
 import "./../styles/tailwind.css";
@@ -8,10 +9,9 @@ import "./../styles/tailwind.css";
 class App extends React.Component<Record<string, unknown>, undefined> {
   public render() {
     return (
-      <div className="app bg-red-800">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        {/* <img src={reactLogo.default} height="480" /> */}
+      <div className="app bg-gray-800">
+        <Header />
+        <MemeGenerator />
       </div>
     );
   }
